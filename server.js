31cd0fea21fdf5392eeeecbb994cbc72e9db9cc3
sendurr <sendurr@hotmail.com>
2017-04-02 17:25:34 -0700

    created connect middleware

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+var connect = require("connect");
 var http = require("http");
+
+var validateUser = function(request,response,next){
+    console.log("Validating user");
+    setTimeout(next(),5000);
+}
 
 var onRequest = function(request, response){
     console.log("User made a request ..." + request.url);
@@ -8,6 +14,18 @@ var onRequest = function(request, response){
 
 }
 
-http.createServer(onRequest).listen(3000);
+var createUser = function(request,response,next){
+    console.log("creating new user for "+ request.url);
+    response.writeHead(200,{"Context-Type":"text/plain"});
+    response.write("New user created.");
+    response.end();
+}
+
+var app = connect();
+app.use(validateUser);
+//app.use(onRequest);
+app.use("/newUser",createUser);
+
+http.createServer(app).listen(3000);
 console.log("Server started ...");
 
